60f5724233ab42d452c7b79f99297db722fef21c
BrewingWeasel <weeaseled@gmail.com>
2023-07-07 16:27:36 -0700

    feat: highlight endings

--- analyze.py
+++ analyze.py
@@ -3,7 +3,115 @@ import spacy
 nlp = spacy.load("lt_core_news_lg")
 
 
+ENDINGS = {
+    "Gen": ["io", "o", "os", "ės", "ies", "aus", "ens", "ers", "ių", "ų", "enų", "erų"],
+    "Dat": [
+        "iui",
+        "ui",
+        "iai",
+        "ai",
+        "ei",
+        "eniui",
+        "eriui",
+        "iams",
+        "ams",
+        "oms",
+        "ėms",
+        "ims",
+        "ums",
+        "enims",
+        "erims",
+        "iems",
+    ],
+    "Acc": [
+        "ią",
+        "ą",
+        "ę",
+        "į",
+        "as",
+        "ų",
+        "enį",
+        "erį",
+        "ius",
+        "us",
+        "es",
+        "is",
+        "enis",
+        "eris",
+    ],
+    "Ins": [
+        "iu",
+        "u",
+        "a",
+        "e",
+        "imi",
+        "umi",
+        "eniu",
+        "erimi",
+        "iais",
+        "ais",
+        "omis",
+        "ėms",
+        "ims",
+        "umis",
+        "enims",
+        "erimis",
+    ],
+    "Loc": [
+        "yje",
+        "oje",
+        "ėje",
+        "uje",
+        "enyje",
+        "eryje",
+        "ame",
+        "iuose",
+        "uose",
+        "ose",
+        "ėse",
+        "yse",
+        "enyse",
+        "eryse",
+        "e",
+    ],
+    "Nom": [
+        "as",
+        "is",
+        "ys",
+        "ias",
+        "a",
+        "ė",
+        "us",
+        "uo",
+        "iai",
+        "ai",
+        "os",
+        "ės",
+        "ys",
+        "ūs",
+        "enys",
+        "erys",
+    ],
+}
+
+
 def analyze(text):
     doc = nlp(text)
-    return [(token.text, token.lemma_, token.morph.get("Case"))
-            for token in doc]
+    final_vals = []
+    for token in doc:
+        if token.morph.get("Case"):
+            for i in ENDINGS[token.morph.get("Case")[0]]:
+                if token.text.endswith(i):
+                    final_vals.append((
+                        token.text.rstrip(
+                            i), i, token.lemma_, token.morph.get("Case")
+                    ))
+                    break
+            else:
+                final_vals.append(
+                    ("", token.text, token.lemma_, token.morph.get("Case"))
+                )
+        else:
+            final_vals.append(
+                (token.text, "", token.lemma_, token.morph.get("Case")))
+    return final_vals
